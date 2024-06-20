e5d7b82d3f1ae3316850bbab6a9cd2a4f6e4c8db
YoungseopLee <73778056+YoungseopLee@users.noreply.github.com>
2024-06-20 10:24:48 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        for(char c : my_string.toCharArray()){
+            if(c >= 48 && c <= 57){
+                answer += Character.getNumericValue(c);
+            }
+        }
+        
+        return answer;
+    }
+}
