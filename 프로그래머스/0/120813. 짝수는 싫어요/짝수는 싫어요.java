628848c932df99d0a82d75e4e85a44eabcd57393
YoungseopLee <73778056+YoungseopLee@users.noreply.github.com>
2024-06-20 09:41:10 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.31 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,7 +3,7 @@ import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n) {
         int[] answer = IntStream.range(1, n+1)
-            .filter(i->i%2==1)
+            .filter(i->i%2==1) // 홀수만 필터링
             .toArray();
         
 
