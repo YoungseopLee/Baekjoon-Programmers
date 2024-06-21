cabfe616eeb719e90d021cd8db2d91faf8c0f76a
YoungseopLee <73778056+YoungseopLee@users.noreply.github.com>
2024-06-21 09:40:24 +0900

    [level 0] Title: 배열 자르기, Time: 2.99 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        List<Integer> list = new ArrayList<>(); 
+        for(int i=num1; i<=num2; i++) list.add(numbers[i]);
+        
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+       
+        
+        
+        return answer;
+    }
+}
