43c1122acb8586357b25824b8fc223822d103c65
YoungseopLee <73778056+YoungseopLee@users.noreply.github.com>
2024-06-25 14:08:50 +0900

    [level 0] Title: 배열 회전시키기, Time: 1.90 ms, Memory: 65.3 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -11,15 +11,12 @@ class Solution {
             nList.add(numbers[numbers.length-1]);
             for(int i=0; i<numbers.length-1; i++)
                 nList.add(numbers[i]);
-            System.out.println(nList);
         }else if(direction.equals("left")){
             for(int i=1; i<numbers.length; i++)
                 nList.add(numbers[i]);
             nList.add(numbers[0]);
-            System.out.println(nList);
         }
         
-        int[] answer = nList.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+        return nList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
