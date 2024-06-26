09dfae286ac846f60ed5db41c063d7c2b5604a75
YoungseopLee <73778056+YoungseopLee@users.noreply.github.com>
2024-06-26 17:22:00 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 4.81 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -5,36 +5,30 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] arr) {
-       
         List<Integer> result = Arrays.stream(arr).boxed().collect(Collectors.toList());
         
         int original = arr.length;
         int modified = getClosestUpperPower(original);
 
-        for(int i=0; i<modified-original; i++){
-            result.add(0);
-        }
+        for(int i=0; i<modified-original; i++) result.add(0);
         
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
     
     public int getClosestUpperPower(int n){
-        
-        if(n <= 0){ // 0 이거나 보다 작으면 1 반환
+        if(n <= 0) // 0 이거나 보다 작으면 1 반환
             return 1;
-        }
         
-        if((n & (n-1)) == 0){ // 이미 거듭제곱이면 그대로 반환 
+        
+        if((n & (n-1)) == 0) // 이미 거듭제곱이면 그대로 반환 
             return n;
-        }
         
         int power = 1; 
-        while(power <= n){ // n 보다 크고 가장 가까운 power 반환
+        
+        while(power <= n) // n 보다 크고 가장 가까운 power 반환
             power *= 2;
-        }
         
         return power;
-        
     }
     
 }
