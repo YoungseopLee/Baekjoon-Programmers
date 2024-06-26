c3e27895c32efd39b01f99a674ee9995c4368509
YoungseopLee <73778056+YoungseopLee@users.noreply.github.com>
2024-06-26 10:35:21 +0900

    [level 0] Title: 글자 지우기, Time: 0.49 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,25 +1,15 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string, int[] indices) {
-        
-        List<Character> list = my_string.chars()
-            .mapToObj(c->(char) c)
-            .collect(Collectors.toList());
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<indices.length; i++){
-            list.set(indices[i], '_');
-        }
 
-        list.removeIf(c->c == '_');
-                  
-        for(Character ch : list){
-            sb.append(ch);
-        }
-             
-        return sb.toString();
+        String[] str = my_string.split("");
+        
+        for(int i=0; i<indices.length; i++)
+            str[indices[i]] = "";
+        
+        return String.join("", str);
     }
 }
